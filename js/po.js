919feb862e9b818915401c54456a9081e446da5f
Ernest Epistola <ernestepistola@gmail.com>
2020-04-23 16:09:00 +0800

    Moved google translate functions to a separate file

--- js/po.js
+++ js/po.js
@@ -94,19 +94,4 @@ function printTextLines() {
     return inputTextLines.join('\n')
 }
 
-async function googleTranslate(text, targetLanguage) {
-    const response = await fetch(`https://translation.googleapis.com/language/translate/v2?key=${apiKey}&target=${targetLanguage}&q=${text}&format=html&model=base`);
-    const data = await response.json()
-    const translatedText = data.data.translations[0].translatedText
-    return translatedText;
-}
-
-function getAvailableLanguages(callback) {
-    const ui = 'en';
-    fetch(`https://translation.googleapis.com/language/translate/v2/languages?key=${apiKey}`).then((data) => {
-        return data.json();
-    }).then(function (json) {
-        callback(json.data.languages)
-    })
-}
 
